33b200776c283df198c1933f8a12946376f99ec1
Neerajpandey2 <128570498+Neerajpandey2@users.noreply.github.com>
2025-02-11 16:07:03 +0530

    db_connection.php

--- PHP/Forum_website1/Partials/db_connection.php
+++ PHP/Forum_website1/Partials/db_connection.php
@@ -1,13 +1,14 @@
-<?php
+//adjust servername , username , password , dbname as per your db . 
+<!-- <?php
 use FTP\Connection;
-             $servername  = "localhost";
-             $username    = "root";
+             $servername  = " ";
+             $username    = " ";
              $password    = "";
-             $dbname     = "idiscuss";
+             $dbname     = " ";
 
               $conn = new mysqli ($servername, $username , $password  , $dbname);
               if($conn->connect_error){
                 die("you can not connect to the database due to ".$conn->connect_error);
               }
 
-?>
+?> -->
